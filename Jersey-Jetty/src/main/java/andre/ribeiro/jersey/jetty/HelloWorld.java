9203ef91f6525249370017ab2a838ffe19d16030
André Ribeiro <andre.ribeiro@ceiia.com>
2017-05-06 12:47:07 +0100

    Initial Commit

---
+++ Jersey-Jetty/src/main/java/andre/ribeiro/jersey/jetty/HelloWorld.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package andre.ribeiro.jersey.jetty;
+
+import com.google.gson.Gson;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ *
+ * @author andreribeiro
+ */
+@Path("/")
+public class HelloWorld {
+    
+    /**
+     * 
+     * @return Hello World in Plain Text. 
+     */
+    @GET
+    @Path("test")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String test() {
+        return "Hello World";
+    }
+    
+    
+    /**
+     * 
+     * @return Hello World in Json format using Gson.
+     */
+    @GET
+    @Path("testJson")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String testJson() {
+        Gson gson = new Gson();
+        return gson.toJson("Hello World");
+    }
+    
+    
+    
+}
